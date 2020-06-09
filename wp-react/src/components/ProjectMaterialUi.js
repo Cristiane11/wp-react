343ec076190a5b1da970ed067fe2c4af535990d2
Cristiane11 <bjwmcd@yahoo.com>
2020-06-08 21:25:49 -0400

    update

---
+++ wp-react/src/components/ProjectMaterialUi.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectMaterialUi = ()=>
+    (<div>
+       
+       function login(...options){
+        console.log(options)
+        }
+            login("facebook",1,2,3,4);
+    </div>)
+
+export default ProjectMaterialUi;
