b7f3d69537dff8758ee66545d0269b23adfaf84d
Cristiane11 <bjwmcd@yahoo.com>
2020-06-09 08:39:58 -0400

    update

--- wp-react/src/components/Home.js
+++ wp-react/src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 import Nav from './Nav';
 const Home = ()=>
     (<div>
         <Nav/>
         <p>This is home</p>
+        <Button variant="contained">
+            Click Me
+        </Button>
     </div>)
 
 export default Home;
